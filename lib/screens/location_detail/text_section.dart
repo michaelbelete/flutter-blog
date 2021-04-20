d1b870b6d6f9673ad19c452d620a7eff57bdd911
FlutLab.io <>
2021-04-20 22:44:55 +0000

    Autosave

---
+++ lib/screens/location_detail/text_section.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'screens/location_detail/location_detail.dart';
+
+class TextSection extends StatelessWidget {
+  final Color _color;
+
+  TextSection(this._color)
+  @override
+  Widget build(BuildContext context) {
+    return Container(decoration: BoxDecoration(color: _color), child: Text("Hello, World"));
+  }
+}
