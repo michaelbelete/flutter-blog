d470a42066764e9d0c3b6a14c647a979e503a828
FlutLab.io <>
2021-04-20 22:28:19 +0000

    Autosave

--- lib/screens/home/location_detail.dart
+++ lib/screens/home/location_detail.dart
@@ -4,14 +4,14 @@ class LocationDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("My, App")),
-      body: Column(
+        appBar: AppBar(title: Text("My, App")),
+        body: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-              Container(decoration: BoxDecoration(color: Colors.red), child: Text("Hello, World")),
-              Container(decoration: BoxDecoration(color: Colors.green), child: Text("Hello, World")),
-              Container(decoration: BoxDecoration(color: Colors.yellow), child: Text("Hello, World")),
+            Container(decoration: BoxDecoration(color: Colors.red), child: Text("Hello, World")),
+            Container(decoration: BoxDecoration(color: Colors.green), child: Text("Hello, World")),
+            Container(decoration: BoxDecoration(color: Colors.yellow), child: Text("Hello, World")),
           ],
-    );
+        ));
   }
 }
