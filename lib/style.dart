8171346f7a9fbb1ef1c6535f6dd8f0f331f99671
FlutLab.io <>
2021-04-25 14:24:45 +0000

    Autosave

--- lib/style.dart
+++ lib/style.dart
@@ -24,5 +24,5 @@ const Body1TextStyle = TextStyle(
   fontFamily: FontNameDefault,
   fontWeight: FontWeight.w300,
   fontSize: BodyTextSize,
-  color: Colors.grey[800],
+  color: Color.grey[800],
 );
