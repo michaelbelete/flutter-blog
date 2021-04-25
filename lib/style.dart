cd632650bf49575330d7073286f246fec3051e7f
FlutLab.io <>
2021-04-25 14:20:50 +0000

    Autosave

--- lib/style.dart
+++ lib/style.dart
@@ -17,12 +17,12 @@ const TitleTextStyle = TextStyle(
   fontFamily: FontNameDefault,
   fontWeight: FontWeight.w300,
   fontSize: LargeTextSize,
-  color: Colors.grey,
+  color: Colors.black,
 );
 
 const Body1TextStyle = TextStyle(
   fontFamily: FontNameDefault,
   fontWeight: FontWeight.w300,
   fontSize: BodyTextSize,
-  color: Colors.grey,
+  color: Colors.black26,
 );
