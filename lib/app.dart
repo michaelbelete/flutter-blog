535426cb7dd3cff2143259fcb3f183ee0f90accd
michael belete <it.michael.belete@gmail.com>
2021-04-26 01:39:44 +0300

    fixed errors

--- lib/app.dart
+++ lib/app.dart
@@ -10,37 +10,39 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        onGenerateRoute: _routes(),
-        home: Locations(),
-        theme: _themeData(),
+      onGenerateRoute: _routes(),
+      home: Locations(),
+      theme: _themeData(),
     );
   }
 
   ThemeData _themeData() {
     return ThemeData(
-            appBarTheme: AppBarTheme(
-              textTheme: TextTheme(title: AppBarTextStyle),
-            ),
-            textTheme: TextTheme(
-              title: TitleTextStyle,
-              body1: Body1TextStyle,
-            ))
+        appBarTheme: AppBarTheme(
+          textTheme: TextTheme(title: AppBarTextStyle),
+        ),
+        textTheme: TextTheme(
+          title: TitleTextStyle,
+          body1: Body1TextStyle,
+        ));
   }
+
   RouteFactory _routes() {
     return (settings) {
       final Map<String, dynamic> arguments = settings.arguments;
       Widget screen;
 
-      switch(settings.name) {
+      switch (settings.name) {
         case LocationRoute:
           screen = Locations();
           break;
         case LocationDetailRoute:
           screen = LocationDetail(arguments['id']);
+          break;
         default:
           return null;
       }
       return MaterialPageRoute(builder: (BuildContext context) => screen);
-    }
+    };
   }
 }
