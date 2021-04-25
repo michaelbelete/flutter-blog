552dc5eb035835117c24f3d736d4417bc9fc8806
FlutLab.io <>
2021-04-25 00:52:24 +0000

    Autosave

--- lib/screens/location_detail/image_banner.dart
+++ lib/screens/location_detail/image_banner.dart
@@ -3,15 +3,14 @@ import 'package:flutter/material.dart';
 class ImageBanner extends StatelessWidget {
   final String _assetPath;
 
-  TextSection(this._assetPath);
+  ImageBanner(this._assetPath);
 
   @override
   Widget build(BuildContext context) {
     return Container(
         constarinsts: BoxConstraints.expand(
-            height: 200.0,
+          height: 200.0,
         ),
-        child: Text("hi")
-    );
+        child: Image.asset(_assetPath));
   }
 }
