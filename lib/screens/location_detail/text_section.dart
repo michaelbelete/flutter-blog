329239ccc2889ea591fde10e53c2571c3633fbe2
FlutLab.io <>
2021-04-25 01:11:51 +0000

    Autosave

--- lib/screens/location_detail/text_section.dart
+++ lib/screens/location_detail/text_section.dart
@@ -3,14 +3,21 @@ import 'package:flutter/material.dart';
 class TextSection extends StatelessWidget {
   final String _title;
   final String _body;
+  static final double hrPadding = 16.0;
 
   TextSection(this._title, this._body);
 
   @override
   Widget build(BuildContext context) {
     return Column(mainAxisAlignment: MainAxisAlignment.start, crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-      Text(_title),
-      Text(_body)
+      Container(
+        padding: const EdgeInsets.fromLTRB(hrPadding, 32.0, hrPadding, 4.0),
+        child: Text(_title),
+      ),
+      Container(
+        padding: const EdgeInsets.fromLTRB(hrPadding, 10.0, hrPadding, hrPadding),
+        child: Text(_body),
+      )
     ]);
   }
 }
